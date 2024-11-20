789836e5e35c3d911be99d4052d6d115f772d0ff
Bruno-rodrigues-farias <brunorodrigues1616@hotmail.com>
2024-11-20 16:38:21 -0300

    Atualizações feitas nas funcionalidades de aluguel de casas

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,14 @@ import cors from "cors";
 
 const app = express();
 
+const port = process.env.PORT || 3333;
+
 app.use(cors());
 
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(port, () => {
     console.log("Server está rodando na porta 3333");
 });
